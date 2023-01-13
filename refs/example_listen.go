cd3cf35deadb2a93a91322525687e82fb603f05d
Mateusz Papuga <mateuszpapuga24@gmail.com>
2023-01-13 19:49:14 +0100

    move examples, add example with new app event

--- refs/example_listen.go
+++ refs/example_listen.go
@@ -1,22 +1,13 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"log"
 	"syscall"
 	"unsafe"
 
-	jw32 "github.com/jcollie/w32"
-	"github.com/tadvi/winc/w32"
 	"golang.org/x/sys/windows"
 )
-
-func prettyPrint(i interface{}) string {
-	s, _ := json.MarshalIndent(i, "", "\t")
-	fmt.Println(s)
-	return ""
-}
 
 var (
 	user32      = windows.NewLazyDLL("user32.dll")
@@ -31,45 +22,7 @@ var (
 	procGetModuleHandle = modkernel32.NewProc("GetModuleHandleW")
 
 	ActiveWinEventHook WINEVENTPROC = func(hWinEventHook HWINEVENTHOOK, event uint32, hwnd HWND, idObject int32, idChild int32, idEventThread uint32, dwmsEventTime uint32) uintptr {
-		log.Print("\n\n\n")
-		log.Println("event:", event)
-		log.Println("hWinEventHook:", hWinEventHook)
-		log.Println("hwnd:", hwnd)
-		log.Println("idObject:", idObject)
-		log.Println("idChild:", idChild)
-		log.Println("idEventThread:", idEventThread)
-		log.Println("dwmsEventTime:", dwmsEventTime)
-
-		isWindow := w32.IsWindow(uintptr(hwnd))
-		log.Println("isWindow: ", isWindow)
-		log.Println("isWindow: ", jw32.IsWindow(jw32.HWND(hwnd)))
-		log.Println("title: ", w32.GetWindowText(uintptr(hwnd)))
-		log.Println("IsWindowEnabled: ", w32.IsWindowEnabled(uintptr(hwnd)))
-
-		a, b := w32.GetWindowThreadProcessId(uintptr(hwnd))
-		log.Println("GetWindowThreadProcessId:", a, b)
-		var windowInfo w32.WINDOWINFO
-		w32.GetWindowInfo(uintptr(hwnd), &windowInfo)
-		fmt.Printf("GetWindowInfo: %+v\n", windowInfo)
-		var windowPlacement w32.WINDOWPLACEMENT
-		w32.GetWindowPlacement(uintptr(hwnd), &windowPlacement)
-		fmt.Printf("GetWindowPlacement: %+v\n", windowPlacement)
-		topWindow := jw32.GetTopWindow(jw32.HWND(hwnd))
-
-		fmt.Printf("GetTopWindow: %#+v\n", topWindow)
-
-		GWL_EXSTYLE := w32.GetWindowLong(uintptr(hwnd), w32.GWL_EXSTYLE)
-		fmt.Printf("GWL_EXSTYLE: %#v\n", GWL_EXSTYLE)
-
-		GWL_STYLE := w32.GetWindowLong(uintptr(hwnd), w32.GWL_STYLE)
-		fmt.Printf("GWL_STYLE: %#v\n", GWL_STYLE)
-		// prettyPrint(windowPlacement)
-
-		GW_OWNER := jw32.GetWindow(jw32.HWND(hwnd), jw32.GW_OWNER)
-		fmt.Printf("GW_OWNER: %#v\n", GW_OWNER)
-
-		fmt.Printf("IsWindowVisible: %#v\n", jw32.IsWindowVisible(jw32.HWND(hwnd)))
-
+		log.Println("fond")
 		return 0
 
 	}
@@ -127,8 +80,8 @@ func main() {
 	hinst := GetModuleHandle("")
 	fmt.Println(hinst)
 
-	winEvHook := SetWinEventHook(0x8000, 0x8000, 0, ActiveWinEventHook, 0, 0, WINEVENT_OUTOFCONTEXT|WINEVENT_SKIPOWNPROCESS)
-	log.Println("ActiveWinEventHook: ", ActiveWinEventHook)
+	winEvHook := SetWinEventHook(EVENT_SYSTEM_FOREGROUND, EVENT_SYSTEM_FOREGROUND, 0, ActiveWinEventHook, 0, 0, WINEVENT_OUTOFCONTEXT|WINEVENT_SKIPOWNPROCESS)
+	log.Println("Windows Event Hook: ")
 	log.Println("Windows Event Hook: ", winEvHook)
 
 	for {
