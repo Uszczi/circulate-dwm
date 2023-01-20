725d374ec2f2c676967b242f947fb5be6b6d8f59
Mateusz Papuga <mateuszpapuga24@gmail.com>
2023-01-20 19:28:23 +0100

    add is window visible

--- circulate/core/debug.go
+++ circulate/core/debug.go
@@ -15,12 +15,14 @@ var (
 
 func PrintDebugWindow(h uintptr) {
 	isWindowIconic, _, _ := isIconic.Call(uintptr(h))
+    isWindowsVisible := w32.IsWindowVisible(h)
 	windowText := w32.GetWindowText(uintptr(h))
 	windowRect := w32.GetWindowRect(uintptr(h))
 	frame, _ := jw32.DwmGetWindowAttribute(jw32.HWND(h), jw32.DWMWA_EXTENDED_FRAME_BOUNDS)
 
 	fmt.Printf("\nGetWindowText: %+v\n", windowText)
 	fmt.Printf("isIconic: %+v\n", isWindowIconic)
+	fmt.Printf("isWindowsVisible: %+v\n", isWindowsVisible)
 	fmt.Printf("GetWindowRect: %+v\n", windowRect)
 	fmt.Printf("DWMWA_EXTENDED_FRAME_BOUNDS: %+v\n\n", frame)
 
