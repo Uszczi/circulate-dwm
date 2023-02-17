c1b93b8715c38294f71fb56724d7fd62d5ff1986
Uszczi <mateuszpapuga24@gmail.com>
2023-02-17 13:06:24 +0100

    working display box

--- circulate.go
+++ circulate.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"circulate/cmd"
+	"circulate/ty"
 	"circulate/win"
+	"fmt"
 	"os"
 	"syscall"
 	"unsafe"
@@ -11,6 +13,14 @@ import (
 )
 
 var kernel32 = syscall.NewLazyDLL("kernel32.dll")
+
+const TRANSPARENCY_COLOUR = 0
+
+// Flags for SetLayeredWindowAttributes.
+const (
+	LWA_COLORKEY = 0x1
+	LWA_ALPHA    = 0x2
+)
 
 var (
 	user32 = syscall.NewLazyDLL("user32.dll")
@@ -42,9 +52,9 @@ const (
 	cWS_OVERLAPPEDWINDOW = 0x00CF0000
 )
 
-func createWindow(className, windowName string, style uint32, x, y, width, height int64, parent, menu, instance syscall.Handle) (syscall.Handle, error) {
+func createWindow(className, windowName string, style uint32, eXStyle uint32, x, y, width, height int64, parent, menu, instance syscall.Handle) (syscall.Handle, error) {
 	ret, _, err := pCreateWindowExW.Call(
-		uintptr(0),
+		uintptr(eXStyle),
 		uintptr(unsafe.Pointer(syscall.StringToUTF16Ptr(className))),
 		uintptr(unsafe.Pointer(syscall.StringToUTF16Ptr(windowName))),
 		uintptr(style),
@@ -162,6 +172,14 @@ func main() {
 	windowText := "CirculateBorder"
 	moduleHandle := win.GetModuleHandle("")
 
+	modgdi32 := syscall.NewLazyDLL("gdi32.dll")
+	user32 := syscall.NewLazyDLL("user32.dll")
+	procCreateSolidBrush := modgdi32.NewProc("CreateSolidBrush")
+	procSetLayeredWindowAttributes := user32.NewProc("SetLayeredWindowAttributes")
+
+	brush, _, _ := procCreateSolidBrush.Call(TRANSPARENCY_COLOUR)
+	fmt.Println(brush)
+
 	fn := func(hwnd syscall.Handle, msg uint32, wparam, lparam uintptr) uintptr {
 		switch msg {
 		case cWM_CLOSE:
@@ -179,7 +197,7 @@ func main() {
 		WndProc:    syscall.NewCallback(fn),
 		Instance:   uintptr(moduleHandle),
 		Cursor:     0,
-		Background: cCOLOR_WINDOW + 1,
+		Background: brush,
 		ClassName:  syscall.StringToUTF16Ptr(className),
 	}
 
@@ -189,7 +207,8 @@ func main() {
 	createWindow(
 		className,
 		windowText,
-		cWS_VISIBLE|cWS_OVERLAPPEDWINDOW,
+		w32.WS_POPUP|w32.WS_SYSMENU|w32.WS_MAXIMIZEBOX|w32.WS_MINIMIZEBOX,
+		w32.WS_EX_TOOLWINDOW|w32.WS_EX_LAYERED,
 		100,
 		100,
 		300,
@@ -198,6 +217,18 @@ func main() {
 		0,
 		syscall.Handle(moduleHandle),
 	)
+
+	var wn ty.HWND
+	callback := func(hwnd ty.HWND) {
+		if win.GetWindowText(hwnd) == "CirculateBorder" {
+			wn = hwnd
+		}
+	}
+	win.EnumWindows(callback)
+
+	procSetLayeredWindowAttributes.Call(uintptr(wn), 0xffffff, 0x255, 1)
+
+	w32.SetWindowPos(w32.HWND(wn), w32.HWND_NOTOPMOST, 100, 100, 100, 100, w32.SWP_SHOWWINDOW|w32.SWP_NOACTIVATE)
 
 	for {
 		msg := tMSG{}
